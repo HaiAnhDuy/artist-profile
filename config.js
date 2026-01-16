47365ecc98508b437739bc38ea5c81557f5f31bb
Hai Anh Duy <haianhduywork@gmail.com>
2026-01-16 22:22:12 +0700

    fix lỗi - admin page 2

---
+++ config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyADHfTbhzGsZR6r6xkYrGC20NkXYYOAmbk",
+  authDomain: "my-portfolio-web-9b21b.firebaseapp.com",
+  projectId: "my-portfolio-web-9b21b",
+  storageBucket: "my-portfolio-web-9b21b.firebasestorage.app",
+  messagingSenderId: "474695357807",
+  appId: "1:474695357807:web:7fb0dfc1278ed8038fc49a",
+  measurementId: "G-YR4CZKGJEN"
+};
+
+// Cloudinary Configuration
+export const CLOUD_NAME = "dul10zzbz";
+export const UPLOAD_PRESET = "my_profile_web";
